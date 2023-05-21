e42596dd1adb5563d36352a71adbc4790b0e897a
Rishabh Singh Sahil <rs4230@srmist.edu.in>
2023-05-21 06:00:57 +0530

    FAQ testimonial added

--- main.js
+++ main.js
@@ -4,3 +4,23 @@ window.addEventListener('scroll', () => {
     document.querySelector('nav').classList.toggle 
     ('window-scroll',window.scrollY > 0)
 })
+
+// FAQ  close and open with javascript
+
+const faq = document.querySelectorAll('.faq');
+
+faq.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open');
+
+        const icon = faq.querySelector('.faq__icon i');
+        if(icon.className === 'uil uil-plus'){
+            icon.className = "uil uil-minus";
+        } else {
+            icon.className = "uil uil-plus";
+        }
+    })
+})
+
+// slider
+
