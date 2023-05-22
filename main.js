ccf93106a7458450ad80dd98a6c247176cace170
Rishabh Singh Sahil <rs4230@srmist.edu.in>
2023-05-23 02:43:33 +0530

    Added Responsiveness for Tablet and Mobile

--- main.js
+++ main.js
@@ -22,5 +22,22 @@ faq.forEach(faq => {
     })
 })
 
-// slider
+// NAV BAR RESPONSIVE show and hide
 
+const menu = document.querySelector(".nav__menu");
+const menuBtn = document.querySelector("#open-menu-btn");
+const closeBtn = document.querySelector("#close-menu-btn");
+
+menuBtn.addEventListener('click' , () => {
+    menu.style.display = "flex";
+    closeBtn.style.display = "inline-block";
+    menuBtn.style.display = "none";
+})
+
+const closeNav = () => {
+    menu.style.display = "none";
+    closeBtn.style.display = "none";
+    menuBtn.style.display = "inline-block";
+}
+
+closeBtn.addEventListener('click' , closeNav)
